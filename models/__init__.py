6909a10f1f970283eac235a621d63e3994f2d3a5
Michelle Anyika <m.anyika@alustudent.com>
2026-01-26 19:49:09 +0200

    Update __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# This file lists all individuals having contributed content to the repository.
+# For syntax, see http://haderech.org/AuthorsFile
+
+Anyika Michelle <m.anyika@alustudent.com>
+Anyika Michelle <anyikamichelle4@gmail.com>
+Emmanuel Atigbi <e.atigbi@alustudent.com>
+Chibueze Onugha <c.onugha@alustudent.com>
